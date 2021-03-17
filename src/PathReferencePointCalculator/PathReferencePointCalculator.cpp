b0157470ba123e77bbde8eb5786f27881f31b54a
Kirill <>
2021-03-17 16:18:38 +0300

    FIX: Fix devide by zero in crossing point calculator. Update reference point finder algorithm. Add horizontal, vertical and angle line paths. Add tests for reference point calculator.

--- src/PathReferencePointCalculator/PathReferencePointCalculator.cpp
+++ src/PathReferencePointCalculator/PathReferencePointCalculator.cpp
@@ -1,5 +1,6 @@
-#include "PathReferncePointCalculator.h"
+#include <math.h>
 #include "GuidingPathPointFinder.h"
+#include "PathReferncePointCalculator.h"
 #include "PerpendicularLinesCrossingPointCalculator.h"
 
 using namespace path_reference_point_calculator;
@@ -15,6 +16,28 @@ std::optional<PathPoint> PathReferncePointCalculator::pathReferencePoint(const C
     auto gPointIndex = mPathPointFinder->pathPointIndex(carState, mPath);
     if (gPointIndex != 0) {
         referencePoint = mCrossingPointCalculator->crossingPoint(carState, mPath.at(gPointIndex - 1), mPath.at(gPointIndex));
+        if (pointOutOfSegment(referencePoint.value(), mPath.at(gPointIndex - 1), mPath.at(gPointIndex))) {
+            if (squareDistanceToPoint(referencePoint.value(), mPath.at(gPointIndex - 1)) < 
+                squareDistanceToPoint(referencePoint.value(), mPath.at(gPointIndex))) {
+                referencePoint = mPath.at(gPointIndex - 1);
+            } else {
+                referencePoint = mPath.at(gPointIndex);
+            }
+        }
     }
     return referencePoint;
+}
+
+bool PathReferncePointCalculator::pointOutOfSegment(const PathPoint &point, const PathPoint &sStartPoint, 
+                                                    const PathPoint &sFinishPoint) const noexcept {
+    if (point.x > std::max(sStartPoint.x, sFinishPoint.x) || point.x < std::min(sStartPoint.x, sFinishPoint.x) ||
+        point.y > std::max(sStartPoint.y, sFinishPoint.y) || point.y < std::min(sStartPoint.y, sFinishPoint.y)) {
+        return true;
+    }
+    return false;
+}
+
+float PathReferncePointCalculator::squareDistanceToPoint(const PathPoint &point1, 
+                                                         const PathPoint &point2) const noexcept {
+    return pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2);
 }
